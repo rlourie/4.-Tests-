74162cbf979a1ee1b8af5e97656acaf5cdffb8b3
rlourie <super.sanek20142@yandex.ru>
2022-02-23 14:06:28 +0300

    Update

--- tests/tests_accounting.py
+++ tests/tests_accounting.py
@@ -1,7 +1,9 @@
 import pytest
+import unittest
 import builtins
 import mock
 from src import accounting
+from unittest.mock import patch
 
 
 class TestFunctions:
@@ -10,8 +12,10 @@ class TestFunctions:
             assert accounting.get_doc_owner_name() == 'Геннадий Покемонов'
         with mock.patch.object(builtins, 'input', lambda _: '2207 876234'):
             assert accounting.get_doc_owner_name() == 'Василий Гупкин'
+        with mock.patch.object(builtins, 'input', lambda _: '10006'):
+            assert accounting.get_doc_owner_name() == 'Аристарх Павлов'
         with mock.patch.object(builtins, 'input', lambda _: '2207 87634'):
-            assert accounting.get_doc_owner_name() == None
+            assert accounting.get_doc_owner_name() is None
 
     def test_get_all_doc_owners_names(self):
         data = []
@@ -35,11 +39,14 @@ class TestFunctions:
             assert accounting.delete_doc() is None
 
     def test_get_doc_shelf(self):
-        with mock.patch.object(builtins, 'input', lambda _: '11-2'):
-            assert accounting.get_doc_shelf() == '1'
-        with mock.patch.object(builtins, 'input', lambda _: '11-3'):
+        with mock.patch.object(builtins, 'input', lambda _: '10006'):
+            assert accounting.get_doc_shelf() == '2'
+        with mock.patch.object(builtins, 'input', lambda _: '115461'):
             assert accounting.get_doc_shelf() is None
 
-    def test_new_doc(self):
-        with mock.patch.object(builtins, 'input', lambda ____: '15', '2', '3', '4'):
-            assert accounting.add_new_doc() == 1
+    @patch('builtins.input', side_effect=['45678', 'passport', 'Арсений Крылов', '3'])
+    def test_add_new_doc(self, mock_input):
+        assert accounting.add_new_doc() == '3'
+        assert len(accounting.documents) == 4
+        assert accounting.documents[3].get("name") == 'Арсений Крылов'
+        assert accounting.directories.get("3") == ['45678']
